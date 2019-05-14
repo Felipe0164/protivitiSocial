479be4955cb474744148a7887256a40ef6914a54
Felipe Alvino <felipe.0164@gmail.com>
2019-05-14 16:27:44 -0300

    Alterações nas rotas

--- routes/oportunidade.ts
+++ routes/oportunidade.ts
@@ -1,39 +1,52 @@
 ﻿import express = require("express");
 import wrap = require("express-async-error-wrapper");
 import Usuario = require("../models/usuario");
+import Oportunidade = require("../models/oportunidade");
 
 const router = express.Router();
 
 router.all("/criar", wrap(async (req: express.Request, res: express.Response) => {
-	let u = await Usuario.cookie(req);
-	if (!u || !u.admin) {
-		res.redirect("/acesso");
-	} else {
-        res.render("negocios/indicacaoOportunidade/alterar", { titulo: "Criar Indição de Oportunidade", usuario:u});
-	}
+    let u = await Usuario.cookie(req);
+    if (!u || !u.admin) {
+        res.redirect("/acesso");
+    } else {
+        res.render("negocios/oportunidade/alterar", {
+            titulo: "Criar Oportunidade",
+            usuario: u,
+            item: null
+        });
+    }
 }));
 
 router.all("/alterar", wrap(async (req: express.Request, res: express.Response) => {
-	let u = await Usuario.cookie(req);
-	if (!u || !u.admin) {
-		res.redirect("/acesso");
-	} else {
-		let id = parseInt(req.query["id"]);
-		let item: Usuario = null;
-		if (isNaN(id) || !(item = await Usuario.obter(id)))
-			res.render("shared/nao-encontrado");
-		else
-            res.render("negocios/indicacaoOportunidade/alterar", { titulo: "Editar Indição de Oportunidade", usuario:u});
-	}
+    let u = await Usuario.cookie(req);
+    if (!u || !u.admin) {
+        res.redirect("/acesso");
+    } else {
+        let id_oportunidade = parseInt(req.query["id_oportunidade"]);
+        let item: Oportunidade = null;
+        if (isNaN(id_oportunidade) || !(item = await Oportunidade.obter(id_oportunidade)))
+            res.render("shared/nao-encontrado", { usuario: u });
+        else
+            res.render("negocios/oportunidade/alterar", {
+                titulo: "Editar Oportunidade",
+                usuario: u,
+                item: item
+            });
+    }
 }));
 
 router.get("/listar", wrap(async (req: express.Request, res: express.Response) => {
-	let u = await Usuario.cookie(req);
-	if (!u || !u.admin) {
-		res.redirect("/acesso");
-	} else {
-        res.render("negocios/indicacaoOportunidade/listar", { titulo: "Visualizar Indição de Oportunidade", usuario:u});
-	}
+    let u = await Usuario.cookie(req);
+    if (!u || !u.admin) {
+        res.redirect("/acesso");
+    } else {
+        res.render("negocios/oportunidade/listar", {
+            titulo: "Gerenciar Oportunidades",
+            usuario: u,
+            lista: JSON.stringify(await Oportunidade.listar())
+        });
+    }
 }));
 
 export = router;
